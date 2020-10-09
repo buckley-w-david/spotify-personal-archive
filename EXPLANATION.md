9f7e272b389cbaaadb5b3bf66d16e976ad4e0867
David Buckley <buckley.w.david@gmail.com>
2020-10-09 17:57:05 -0400

    Github action and pull.py are correct

---
+++ EXPLANATION.md
@@ -0,0 +1,5 @@
+# Explanation 
+
+The idea of this is scrape my weekly playlists from Spotify and store them in a git repo
+
+https://simonwillison.net/2020/Oct/9/git-scraping/
--- pull.py
+++ pull.py
@@ -6,22 +6,24 @@ from urllib.parse import urljoin
 SPOTIFY_PREFIX='https://open.spotify.com'
 config = toml.load("config.toml")
 
-playlist_url = urljoin(urljoin(SPOTIFY_PREFIX, '/playlist/'), config['playlist'])
-with urlopen(playlist_url) as request:
-    soup = BeautifulSoup(request.read(), features="html.parser")
+print(f"# My Playlists")
+for playlist in config["Playlists"]:
+    playlist_url = urljoin(urljoin(SPOTIFY_PREFIX, '/playlist/'), playlist['id'])
+    with urlopen(playlist_url) as request:
+        soup = BeautifulSoup(request.read(), features="html.parser")
 
-print(f"# {config['name']}")
-for track in soup.find_all(class_='tracklist-col name'):
-    *artists, song = track.find_all('a')
-    artist_entries = []
-    # import pdb; pdb.set_trace()
+    print(f"# {playlist['name']}")
+    for track in soup.find_all(class_='tracklist-col name'):
+        *artists, album = track.find_all('a')
+        artist_entries = []
 
-    for artist in artists:
-        artist_url = urljoin(SPOTIFY_PREFIX, song.get('href'))
-        artist_entry = f"[{artist.text}]({artist_url})"
-        artist_entries.append(artist_entry)
+        for artist in artists:
+            artist_url = urljoin(SPOTIFY_PREFIX, artist.get('href'))
+            artist_entry = f"[{artist.text}]({artist_url})"
+            artist_entries.append(artist_entry)
 
-    song_url = urljoin(SPOTIFY_PREFIX, song.get('href'))
-    song_entry = f"[{song.text}]({song_url})"
+        album_url = urljoin(SPOTIFY_PREFIX, album.get('href'))
+        album_entry = f"[{album.text}]({album_url})"
 
-    print(f" - {song_entry} - {', '.join(artist_entries)}")
+        song_entry = track.find('span').text
+        print(f" - {song_entry} from {album_entry} by {', '.join(artist_entries)}")
